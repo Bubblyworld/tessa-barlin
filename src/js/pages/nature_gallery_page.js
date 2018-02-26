f8055716dde9f8b7d078c9470243c57b29586025
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2018-02-26 12:00:23 +0200

    Move to smaller images in the galleries for now.

--- src/js/pages/nature_gallery_page.js
+++ src/js/pages/nature_gallery_page.js
@@ -7,24 +7,24 @@ export default class NatureGalleryPage extends React.Component {
         let logo = 'img/elephant_logo_gold.png';
         let images = [
             [
-                { url: 'img/nature/1-1.jpg', width: 2000, height: 3000 },
-                { url: 'img/nature/1-2.jpg', width: 2606, height: 3648 }
+                { url: 'img/nature/1-1_sma.jpg', width: 2000, height: 3000 },
+                { url: 'img/nature/1-2_sma.jpg', width: 2606, height: 3648 }
             ],
             [
-                { url: 'img/nature/2-1.jpg', width: 3000, height: 1445 }
+                { url: 'img/nature/2-1_sma.jpg', width: 3000, height: 1445 }
             ],
             [
-                { url: 'img/nature/3-1.jpg', width: 3000, height: 2000 },
-                { url: 'img/nature/3-2.jpg', width: 3000, height: 2000 }
+                { url: 'img/nature/3-1_sma.jpg', width: 3000, height: 2000 },
+                { url: 'img/nature/3-2_sma.jpg', width: 3000, height: 2000 }
             ],
             [
-                { url: 'img/nature/4-1.jpg', width: 3000, height: 2000 },
-                { url: 'img/nature/4-2.jpg', width: 3000, height: 2000 }
+                { url: 'img/nature/4-1_sma.jpg', width: 3000, height: 2000 },
+                { url: 'img/nature/4-2_sma.jpg', width: 3000, height: 2000 }
             ],
             [
-                { url: 'img/nature/5-1.jpg', width: 1667, height: 2500 },
-                { url: 'img/nature/5-2.jpg', width: 2253, height: 3000 },
-                { url: 'img/nature/5-3.jpg', width: 2091, height: 2927 }
+                { url: 'img/nature/5-1_sma.jpg', width: 1667, height: 2500 },
+                { url: 'img/nature/5-2_sma.jpg', width: 2253, height: 3000 },
+                { url: 'img/nature/5-3_sma.jpg', width: 2091, height: 2927 }
             ]
         ];
 
--- src/js/pages/portraiture_gallery_page.js
+++ src/js/pages/portraiture_gallery_page.js
@@ -7,30 +7,30 @@ export default class PortraitureGalleryPage extends React.Component {
         let logo = 'img/flower_logo_gold.png';
         let images = [
             [
-                { url: 'img/portraiture/1-1.jpg', width: 2500, height: 1667 }
+                { url: 'img/portraiture/1-1_sma.jpg', width: 2500, height: 1667 }
             ],
             [
-                { url: 'img/portraiture/2-1.jpg', width: 2500, height: 1667 },
-                { url: 'img/portraiture/2-2.jpg', width: 5472, height: 3648 }
+                { url: 'img/portraiture/2-1_sma.jpg', width: 2500, height: 1667 },
+                { url: 'img/portraiture/2-2_sma.jpg', width: 5472, height: 3648 }
             ],
             [
-                { url: 'img/portraiture/3-1.jpg', width: 3000, height: 2000 }
+                { url: 'img/portraiture/3-1_sma.jpg', width: 3000, height: 2000 }
             ],
             [
-                { url: 'img/portraiture/4-1.jpg', width: 2000, height: 3000 },
-                { url: 'img/portraiture/4-2.jpg', width: 2000, height: 3000 },
-                { url: 'img/portraiture/4-3.jpg', width: 3648, height: 5472 }
+                { url: 'img/portraiture/4-1_sma.jpg', width: 2000, height: 3000 },
+                { url: 'img/portraiture/4-2_sma.jpg', width: 2000, height: 3000 },
+                { url: 'img/portraiture/4-3_sma.jpg', width: 3648, height: 5472 }
             ],
             [
-                { url: 'img/portraiture/5-1.jpg', width: 1667, height: 2500 },
-                { url: 'img/portraiture/5-2.jpg', width: 3648, height: 5472 }
+                { url: 'img/portraiture/5-1_sma.jpg', width: 1667, height: 2500 },
+                { url: 'img/portraiture/5-2_sma.jpg', width: 3648, height: 5472 }
             ],
             [
-                { url: 'img/portraiture/6-1.jpg', width: 3000, height: 2066 }
+                { url: 'img/portraiture/6-1_sma.jpg', width: 3000, height: 2066 }
             ],
             [
-                { url: 'img/portraiture/7-1.jpg', width: 2074, height: 3000 },
-                { url: 'img/portraiture/8-1.jpg', width: 2000, height: 3000 }
+                { url: 'img/portraiture/7-1_sma.jpg', width: 2074, height: 3000 },
+                { url: 'img/portraiture/8-1_sma.jpg', width: 2000, height: 3000 }
             ]
         ];
 
