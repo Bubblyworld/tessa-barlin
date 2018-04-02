0d4b16c4f4747fe31dd63f7213cde44386c239aa
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2018-04-02 12:56:07 +0200

    Fix gallery for portraiture.

--- src/js/pages/portraiture_gallery_page.js
+++ src/js/pages/portraiture_gallery_page.js
@@ -8,7 +8,7 @@ export default class PortraitureGalleryPage extends React.Component {
         let story  = 'Every shot tells a story.';
         let images = [
             [
-                { urls: ['img/portraiture/1-1-1.jpg'], width: 1667, height: 2500 }
+                { urls: ['img/portraiture/1-1-1.jpg', 'img/portraiture/1-1-2.jpg', 'img/portraiture/1-1-3.jpg'], width: 1667, height: 2500 }
             ],
             [
                 { urls: ['img/portraiture/2-1-1.jpg', 'img/portraiture/2-1-2.jpg'], width: 2500, height: 1667 }
